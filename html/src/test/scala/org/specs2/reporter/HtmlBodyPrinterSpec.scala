4b8285b0120fabc861f1c81191521b33b4fc39f1
SemanticBeeng <nick@semanticbeeng.com>
2016-08-30 01:14:42 -0700

    merged upstream

--- html/src/test/scala/org/specs2/reporter/HtmlBodyPrinterSpec.scala
+++ html/src/test/scala/org/specs2/reporter/HtmlBodyPrinterSpec.scala
@@ -4,16 +4,24 @@ package reporter
 import io.DirectoryPath
 import main.Arguments
 import specification.process.Level
-import specification.core.Fragment
+import org.specs2.specification.core.Fragment
+import org.specs2.specification.create.DefaultFragmentFactory
 
 object HtmlBodyPrinterSpec extends Specification { def is = s2"""
 
  A hidden reference must not be printed $hidden
 
+ A snippet be printed $snippet
+
 """
 
   def hidden = {
     print(link(HtmlBodyPrinterSpec).hide) must beEmpty
+  }
+
+  def snippet = {
+    //print(DefaultFragmentFactory.code("val a = 123")) must_== "val a = 123"
+    success
   }
 
   def print(f: Fragment) =
